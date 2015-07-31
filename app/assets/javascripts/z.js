6fe7254fa4982e92878a5b13dd7d091c4c472554
mike vallano <mikevallano@gmail.com>
2015-07-31 08:22:26 -0500

    Added a bit of javascript to kitties/new

--- app/assets/javascripts/z.js
+++ app/assets/javascripts/z.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
-
-
   console.log("i am in yer javascript console")
 
+  $('#kitty-form').hide();
+
+  $('#add-kitty').on("click", function() {
+    $('#kitty-form').slideDown();
+  });
 
 
 // the final closer
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150731131102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "kitties", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.integer  "cuteness"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
